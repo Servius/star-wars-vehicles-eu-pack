aff3549a3cec4ba8cdd8511daa4afe0d4059c478
Servius <nosamrj@gmail.com>
2017-04-07 21:09:53 -0700

    Changelog updated

--- star_wars_vehicles_eu_addon/lua/autorun/server/init.lua
+++ star_wars_vehicles_eu_addon/lua/autorun/server/init.lua
@@ -1,10 +1,8 @@
 local function EUPlug( )
 
-print("Star Wars Vehicles EU Collection Changelog 1.0")
+print("Star Wars Vehicles EU Collection Changelog 1.1")
 print("===MOST RECENT UPDATE===")
-print("- Added this on spawn console message to help inform people what has changed with this addon.")
-print("- Added support for both ship packs made by thd 4/1/2017")
-print("- Added a third category for the new batch of ships. Due to the amount of vehicles I would like have more input on what the mechanics for these ships should be before moved to the final section.")
+print("- 20 new ships added.")
 print("NOTICE: If you are interested in developing this addon feel free to take a look at the git repository located here:")
 print("https://github.com/Servius/star-wars-vehicles-eu-pack")
 print("I'd love the opportunity to work with any of you talented peeps out there -Servius")
