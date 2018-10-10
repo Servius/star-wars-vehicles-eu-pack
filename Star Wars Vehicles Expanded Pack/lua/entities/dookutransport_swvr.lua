acaaa91bf545cf943228430f0f939aaeaf07c2fd
Servius <nosamrj@yahoo.com>
2018-10-09 17:51:30 -0700

    Conversion Progress; Notes added.

--- Star Wars Vehicles Expanded Pack/lua/entities/dookutransport_swvr.lua
+++ Star Wars Vehicles Expanded Pack/lua/entities/dookutransport_swvr.lua
@@ -1,9 +1,9 @@
 ENT.Base = "swvr_base"
 
 ENT.Category = "CIS"
-ENT.Class = "Fighter"
+ENT.Class = "Transport"
 
-ENT.PrintName = "Belbullab-22 Starfighter"
+ENT.PrintName = "Punworcca 116 Transport"
 ENT.Author = "Servius"
 
 if SERVER then
@@ -15,7 +15,7 @@ if SERVER then
         end
 
         local ent = ents.Create(ClassName)
-        ent:SetPos(tr.HitPos + Vector(0, 0, 100)) -- Third number controls height. Negative equals down, postive equals up.
+        ent:SetPos(tr.HitPos + Vector(0, 0, 200)) -- Third number controls height. Negative equals down, postive equals up.
         ent:SetAngles(Angle(0, ply:GetAimVector():Angle().Yaw, 0))
         ent:Spawn()
         ent:Activate()
@@ -25,7 +25,7 @@ if SERVER then
 
     function ENT:Initialize()
         self:Setup({
-            Model = "models/ship_belbu/ship_belbu_servius.mdl",
+            Model = "models/ship_dooku/ship_dooku_servius.mdl",
             Health = 1500,
             Speed = 1250,
             Shields = 1000,
@@ -33,21 +33,14 @@ if SERVER then
             VerticalSpeed = 600,
             Acceleration = 8,
             Roll = true,
-            LandVector = Vector(0, 0, 60) -- Third number is up/down. 
         })
 
         self:AddWeaponGroup("Pilot", "ls1_cannon", {
-            Delay = 0.2,
-            Damage = 75,
-            CanOverheat = true,
-            MaxOverheat = 20
         })
 
-        self:AddWeapon("Pilot", "Left", Vector(150, -75, 30))
-        self:AddWeapon("Pilot", "Right", Vector(150, 75, 30))
+
 
         self:AddPilot(nil, nil, {
-            Weapons = { "Pilot" },
             ExitPos = Vector(-200, -150, 0)
         })
 
@@ -65,21 +58,7 @@ if CLIENT then
 
         self:SetupDefaults()
 
-        self:AddEngine(Vector(-190, 35, 0), {
-            StartSize = 15,
-            EndSize = 13.5,
-            Lifetime = 2.7,
-            Color = Color(150, 100, 0),
-            Sprite = "sprites/orangecore1"
-        })
 
-        self:AddEngine(Vector(-190, -35, 0), {
-            StartSize = 15,
-            EndSize = 13.5,
-            Lifetime = 2.7,
-            Color = Color(150, 100, 0),
-            Sprite = "sprites/orangecore1"
-        })
 
         self.BaseClass.Initialize(self)
     end
